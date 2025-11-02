70d36e5fd6720ae66dd1e1e0f0d8d9ed651038b4
alifahmed007 <alifahammead001@gmail.com>
2025-11-02 11:26:22 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Countries from './components/countries/countries'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <h1>React world on the go..</h1>
+      <Countries></Countries>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/countries/countries.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const  Countries =()=>{
+    return (
+        <div>
+            <h4>In the Countries</h4>
+        </div>
+    );
+};
+
+export default Countries;
